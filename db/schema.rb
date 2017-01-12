36f5b207dd7d94d13b2908aa52816f9e13c21274
Xander's sample blog <alexdbondoc@gmail.com>
2017-01-12 14:56:59 +0800

    Sign up user with password in UI

--- db/schema.rb
+++ db/schema.rb
@@ -23,8 +23,9 @@ ActiveRecord::Schema.define(version: 20170112033311) do
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
