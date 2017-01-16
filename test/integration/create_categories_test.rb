b796178f1dc71e5cf3aee049deeedabc2328c636
Xander's sample blog <alexdbondoc@gmail.com>
2017-01-16 09:57:03 +0800

    Fix tests and update nav navbar with categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,27 +1,31 @@
 require 'test_helper'
 
-class CreateCategoriesTest < ActionDispatch::IntegrationTest
-
-	test "get new category form and create category" do
-		get new_category_path
-		assert_template 'categories/new'
-		assert_difference 'Category.count', 1 do
-			post_via redirect categories_path, category: {name: "sports"}
-		end
-		assert_template 'categories/index'
-		assert_match "sports", response.body
-	end
-
-	test "invalid category submission results in failure" do
-		sign_in_as(@user, "password")
-		get new_category_path
-		assert_template 'categories/new'
-		assert_no_difference 'Category.count' do
-			post categories_path, category: { name: " " }
-		end
-		assert_template 'categories/new'
-		assert_select 'h2.panel-title'
-		assert_select 'div.panel-body'
-	end
-
+class CategoriesControllerTest < ActionController::TestCase
+  def setup
+    @category = Category.create(name: "sports")
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+  end
+  
+  test "should get categories index" do
+    get :index
+    assert_response :success
+  end
+  
+  test "should get new" do
+    session[:user_id] = @user.id
+    get :new
+    assert_response :success
+  end
+  
+  test "should get show" do
+    get(:show, {'id' => @category.id})
+    assert_response :success
+  end
+  
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: { name: "sports" }
+    end
+    assert_redirected_to categories_path
+  end
 end
